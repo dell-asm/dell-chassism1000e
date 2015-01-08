d172511b88faff796baea0efa877b22aa502114b
Adan Saenz <asaenz@momentumsi.com>
2015-01-08 11:06:24 -0600

    Removed files and added deprecation message to README

--- manifests/params.pp
+++
@@ -1,7 +0,0 @@
-#Paramaters class for chassism1000e
-class chassism1000e::params {
-  $owner    = 'pe-puppet'
-  $group    = 'pe-puppet'
-  $provider = 'gem'
-  $mode     = '0644'
-}
--- spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'chassism1000e' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('chassism1000e') }
-  end
-end
